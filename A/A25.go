a8f3d9ebc5ad27e8be61ce5bc98c46d4ecb4a797
taisei <hutoboshi.eng@gmail.com>
2024-03-05 22:35:28 +0900

    25

---
+++ A/A25.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func main() {
+	var h, w int
+	fmt.Scan(&h, &w)
+
+	arrC := make([]string, h)
+	for i := 0; i < h; i++ {
+		fmt.Scan(&arrC[i])
+	}
+
+	dp := make([][]int64, h)
+	for i := range dp {
+		dp[i] = make([]int64, w)
+	}
+
+	for i := 0; i < h; i++ {
+		for j := 0; j < w; j++ {
+			if i == 0 && j == 0 {
+				dp[i][j] = 1
+			} else {
+				dp[i][j] = 0
+				if i >= 1 && arrC[i-1][j] == '.' {
+					dp[i][j] += dp[i-1][j]
+				}
+				if j >= 1 && arrC[i][j-1] == '.' {
+					dp[i][j] += dp[i][j-1]
+				}
+			}
+		}
+	}
+
+	fmt.Println(dp[h-1][w-1])
+	fmt.Println(dp)
+}
