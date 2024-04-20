43c454efca6d5c1172bedef04726a0e6684a757b
taisei <hutoboshi.eng@gmail.com>
2024-04-20 15:33:00 +0900

    幅優先探索

---
+++ atcoder/ABC168/D.go
@@ -0,0 +1,158 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Io struct {
+	reader    *bufio.Reader
+	writer    *bufio.Writer
+	tokens    []string
+	nextToken int
+}
+
+func NewIo() *Io {
+	return &Io{
+		reader: bufio.NewReader(os.Stdin),
+		writer: bufio.NewWriter(os.Stdout),
+	}
+}
+
+func (io *Io) Flush() {
+	err := io.writer.Flush()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (io *Io) NextLine() string {
+	var buffer []byte
+	for {
+		line, isPrefix, err := io.reader.ReadLine()
+		if err != nil {
+			panic(err)
+		}
+		buffer = append(buffer, line...)
+		if !isPrefix {
+			break
+		}
+	}
+	return string(buffer)
+}
+
+func (io *Io) Next() string {
+	for io.nextToken >= len(io.tokens) {
+		line := io.NextLine()
+		io.tokens = strings.Fields(line)
+		io.nextToken = 0
+	}
+	r := io.tokens[io.nextToken]
+	io.nextToken++
+	return r
+}
+
+func (io *Io) NextInt() int {
+	i, err := strconv.Atoi(io.Next())
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
+func (io *Io) NextFloat() float64 {
+	i, err := strconv.ParseFloat(io.Next(), 64)
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
+func (io *Io) PrintLn(a ...interface{}) {
+	fmt.Fprintln(io.writer, a...)
+}
+
+func (io *Io) Printf(format string, a ...interface{}) {
+	fmt.Fprintf(io.writer, format, a...)
+}
+
+func (io *Io) PrintIntLn(a []int) {
+	b := []interface{}{}
+	for _, x := range a {
+		b = append(b, x)
+	}
+	io.PrintLn(b...)
+}
+
+func (io *Io) PrintStringLn(a []string) {
+	b := []interface{}{}
+	for _, x := range a {
+		b = append(b, x)
+	}
+	io.PrintLn(b...)
+}
+
+func Log(name string, value interface{}) {
+	fmt.Fprintf(os.Stderr, "%s=%+v\n", name, value)
+}
+
+func intMin(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func intMax(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func main() {
+	//決まり文句
+	io := NewIo()
+	defer io.Flush()
+
+	n := io.NextInt() //頂点の数
+	m := io.NextInt() //辺の数
+
+	//隣接リストの初期化
+	connect := make([][]int, n+1)
+
+	//辺の情報を読み込んで隣接リストを作成
+	for i := 0; i < m; i++ {
+		a := io.NextInt()
+		b := io.NextInt()
+		connect[a] = append(connect[a], b)
+		connect[b] = append(connect[b], a)
+	}
+
+	//幅優先探索で最短経路を求める
+	visited := make([]bool, n+1)
+	visited[1] = true
+
+	que := []int{1}
+	ans := make([]int, n+1)
+
+	for len(que) > 0 {
+		nowRoom := que[0]
+		que = que[1:]
+		for _, toRoom := range connect[nowRoom] {
+			if !visited[toRoom] {
+				visited[toRoom] = true
+				ans[toRoom] = nowRoom
+				que = append(que, toRoom)
+			}
+		}
+	}
+
+	fmt.Println("Yes")
+	for i := 2; i <= n; i++ {
+		fmt.Println(ans[i])
+	}
+}
