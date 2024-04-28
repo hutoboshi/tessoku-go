6dcedb03ed7c7e67e594a8577734091f21e225dd
taisei <hutoboshi.eng@gmail.com>
2024-04-28 21:37:51 +0900

    高さ・幅

--- atcoder/ABC213/C.go
+++ atcoder/ABC213/C.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -117,5 +118,55 @@ func main() {
 	//決まり文句
 	io := NewIo()
 	defer io.Flush()
+
+	n := io.NextInt()
+	n = io.NextInt()
+	n = io.NextInt()
+
+	//点の座標を保持するスライスと高さ、幅のリストを初期化
+	var points [][]int
+	var heightList, widthList []int
+
+	//高さと幅のマップを初期化
+	heightMap := make(map[int]int)
+	widthMap := make(map[int]int)
+
+	//点の座標を入力し、高さと幅の重複を除いてリストに追加
+	for i := 0; i < n; i++ {
+		x := io.NextInt()
+		y := io.NextInt()
+
+		//高さの重複を除いてリストに追加
+		if _, ok := heightMap[x]; !ok {
+			heightMap[x] = 1
+			heightList = append(heightList, x)
+		}
+
+		//幅の重複を除いてリストを追加
+		if _, ok := widthMap[y]; !ok {
+			widthMap[y] = 1
+			widthList = append(widthList, y)
+		}
+
+		//点の座標をcに追加
+		points = append(points, []int{x, y})
+	}
+
+	//高さ・幅のリストをソート
+	sort.Ints(heightList)
+	sort.Ints(widthList)
+
+	//高さと幅のマップをインデックスに変換する
+	for i := 0; i < len(heightList); i++ {
+		heightMap[heightList[i]] = i
+	}
+	for i := 0; i < len(widthList); i++ {
+		widthMap[widthList[i]] = i
+	}
+
+	//結果を出力
+	for _, v := range points {
+		fmt.Println(heightMap[v[0]]+1, widthMap[v[1]]+1)
+	}
 
 }
