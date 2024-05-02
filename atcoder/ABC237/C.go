a6319dd8c45a0029bc4cc4999ca5370ac092e49c
taisei <hutoboshi.eng@gmail.com>
2024-05-02 18:02:54 +0900

    新聞紙文字問題

---
+++ atcoder/ABC237/C.go
@@ -0,0 +1,161 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Io struct {
+	reader    *bufio.Reader
+	writer    *bufio.Writer
+	tokens    []string
+	nextToken int
+}
+
+func NewIo() *Io {
+	return &Io{
+		reader: bufio.NewReader(os.Stdin),
+		writer: bufio.NewWriter(os.Stdout),
+	}
+}
+
+func (io *Io) Flush() {
+	err := io.writer.Flush()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (io *Io) NextLine() string {
+	var buffer []byte
+	for {
+		line, isPrefix, err := io.reader.ReadLine()
+		if err != nil {
+			panic(err)
+		}
+		buffer = append(buffer, line...)
+		if !isPrefix {
+			break
+		}
+	}
+	return string(buffer)
+}
+
+func (io *Io) Next() string {
+	for io.nextToken >= len(io.tokens) {
+		line := io.NextLine()
+		io.tokens = strings.Fields(line)
+		io.nextToken = 0
+	}
+	r := io.tokens[io.nextToken]
+	io.nextToken++
+	return r
+}
+
+func (io *Io) NextInt() int {
+	i, err := strconv.Atoi(io.Next())
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
+func (io *Io) NextFloat() float64 {
+	i, err := strconv.ParseFloat(io.Next(), 64)
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
+func (io *Io) PrintLn(a ...interface{}) {
+	fmt.Fprintln(io.writer, a...)
+}
+
+func (io *Io) Printf(format string, a ...interface{}) {
+	fmt.Fprintf(io.writer, format, a...)
+}
+
+func (io *Io) PrintIntLn(a []int) {
+	b := []interface{}{}
+	for _, x := range a {
+		b = append(b, x)
+	}
+	io.PrintLn(b...)
+}
+
+func (io *Io) PrintStringLn(a []string) {
+	b := []interface{}{}
+	for _, x := range a {
+		b = append(b, x)
+	}
+	io.PrintLn(b...)
+}
+
+func Log(name string, value interface{}) {
+	fmt.Fprintf(os.Stderr, "%s=%+v\n", name, value)
+}
+
+func intMin(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func intMax(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+// 文字を逆から読む
+func main() {
+	//決まり文句
+	io := NewIo()
+	defer io.Flush()
+
+	s := io.Next()
+	n := len(s)
+
+	var x int
+	for i := 0; i < n; i++ {
+		if s[i] == 'a' {
+			x++
+		} else {
+			break
+		}
+	}
+
+	var y int
+	for i := n - 1; i >= 0; i-- {
+		if s[i] == 'a' {
+			y++
+		} else {
+			break
+		}
+	}
+
+	if x == n {
+		fmt.Println("Yes")
+		return
+	}
+
+	if x > y {
+		fmt.Println("No")
+		return
+	}
+
+	for i := x; i < (n - y); i++ {
+		if s[i] != s[x+n-y-i-1] {
+			fmt.Println("No")
+			return
+		}
+	}
+	fmt.Println("Yes")
+
+}
