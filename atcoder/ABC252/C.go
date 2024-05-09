883b787d314a04689c788407c7f37b7fa63c630e
taisei <hutoboshi.eng@gmail.com>
2024-05-09 20:35:15 +0900

    slot

---
+++ atcoder/ABC252/C.go
@@ -0,0 +1,159 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Io struct {
+	reader    *bufio.Reader
+	writer    *bufio.Writer
+	tokens    []string
+	nextToken int
+}
+
+func NewIo() *Io {
+	return &Io{
+		reader: bufio.NewReader(os.Stdin),
+		writer: bufio.NewWriter(os.Stdout),
+	}
+}
+
+func (io *Io) Flush() {
+	err := io.writer.Flush()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (io *Io) NextLine() string {
+	var buffer []byte
+	for {
+		line, isPrefix, err := io.reader.ReadLine()
+		if err != nil {
+			panic(err)
+		}
+		buffer = append(buffer, line...)
+		if !isPrefix {
+			break
+		}
+	}
+	return string(buffer)
+}
+
+func (io *Io) Next() string {
+	for io.nextToken >= len(io.tokens) {
+		line := io.NextLine()
+		io.tokens = strings.Fields(line)
+		io.nextToken = 0
+	}
+	r := io.tokens[io.nextToken]
+	io.nextToken++
+	return r
+}
+
+func (io *Io) NextInt() int {
+	i, err := strconv.Atoi(io.Next())
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
+func (io *Io) NextFloat() float64 {
+	i, err := strconv.ParseFloat(io.Next(), 64)
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
+func (io *Io) PrintLn(a ...interface{}) {
+	fmt.Fprintln(io.writer, a...)
+}
+
+func (io *Io) Printf(format string, a ...interface{}) {
+	fmt.Fprintf(io.writer, format, a...)
+}
+
+func (io *Io) PrintIntLn(a []int) {
+	b := []interface{}{}
+	for _, x := range a {
+		b = append(b, x)
+	}
+	io.PrintLn(b...)
+}
+
+func (io *Io) PrintStringLn(a []string) {
+	b := []interface{}{}
+	for _, x := range a {
+		b = append(b, x)
+	}
+	io.PrintLn(b...)
+}
+
+func Log(name string, value interface{}) {
+	fmt.Fprintf(os.Stderr, "%s=%+v\n", name, value)
+}
+
+func intMin(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func intMax(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func main() {
+	//決まり文句
+	io := NewIo()
+	defer io.Flush()
+
+	n := io.NextInt()
+	arrS := make([]string, n)
+	for i := 0; i < n; i++ {
+		arrS[i] = io.NextLine()
+	}
+
+	ans := 1 << 30
+	//targetの数字がどこにあるかまたその個数のmaxを計算
+	for target := '0'; target <= '9'; target++ {
+		targetCounter := make([]int, 10)
+		//一列ごとに確かめる
+		for _, si := range arrS {
+			//１文字ごとに確かめる
+			for i, ch := range si {
+				if ch == target {
+					targetCounter[i]++
+				}
+			}
+		}
+
+		//targetCounterの最大の位置を取得
+		idx := maxElementIndex(targetCounter)
+		//計算
+		ans = intMin(ans, (targetCounter[idx]-1)*10+idx)
+	}
+	fmt.Println(ans)
+}
+
+func maxElementIndex(a []int) int {
+	maxIndex := 0
+	maxElem := a[maxIndex]
+	for i, ai := range a {
+		if maxElem <= ai {
+			maxIndex = i
+			maxElem = ai
+		}
+	}
+	return maxIndex
+}
