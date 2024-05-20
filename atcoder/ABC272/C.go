bf9acf3b779b970a3f8bf188cd8cc2fcb781a4f7
taisei <hutoboshi.eng@gmail.com>
2024-05-20 20:14:11 +0900

    amari

---
+++ atcoder/ABC272/C.go
@@ -0,0 +1,155 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+type Io struct {
+	reader    *bufio.Reader
+	writer    *bufio.Writer
+	tokens    []string
+	nextToken int
+}
+
+func NewIo() *Io {
+	return &Io{
+		reader: bufio.NewReader(os.Stdin),
+		writer: bufio.NewWriter(os.Stdout),
+	}
+}
+
+func (io *Io) Flush() {
+	err := io.writer.Flush()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (io *Io) NextLine() string {
+	var buffer []byte
+	for {
+		line, isPrefix, err := io.reader.ReadLine()
+		if err != nil {
+			panic(err)
+		}
+		buffer = append(buffer, line...)
+		if !isPrefix {
+			break
+		}
+	}
+	return string(buffer)
+}
+
+func (io *Io) Next() string {
+	for io.nextToken >= len(io.tokens) {
+		line := io.NextLine()
+		io.tokens = strings.Fields(line)
+		io.nextToken = 0
+	}
+	r := io.tokens[io.nextToken]
+	io.nextToken++
+	return r
+}
+
+func (io *Io) NextInt() int {
+	i, err := strconv.Atoi(io.Next())
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
+func (io *Io) NextFloat() float64 {
+	i, err := strconv.ParseFloat(io.Next(), 64)
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
+func (io *Io) PrintLn(a ...interface{}) {
+	fmt.Fprintln(io.writer, a...)
+}
+
+func (io *Io) Printf(format string, a ...interface{}) {
+	fmt.Fprintf(io.writer, format, a...)
+}
+
+func (io *Io) PrintIntLn(a []int) {
+	b := []interface{}{}
+	for _, x := range a {
+		b = append(b, x)
+	}
+	io.PrintLn(b...)
+}
+
+func (io *Io) PrintStringLn(a []string) {
+	b := []interface{}{}
+	for _, x := range a {
+		b = append(b, x)
+	}
+	io.PrintLn(b...)
+}
+
+func Log(name string, value interface{}) {
+	fmt.Fprintf(os.Stderr, "%s=%+v\n", name, value)
+}
+
+func intMin(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func intMax(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func main() {
+	//決まり文句
+	io := NewIo()
+	defer io.Flush()
+
+	n := io.NextInt()
+	//偶数
+	arrA := make([]int, 0)
+	//奇数
+	arrB := make([]int, 0)
+	for i := 0; i < n; i++ {
+		tmp := io.NextInt()
+		amari := tmp % 2
+		if amari == 0 {
+			arrA = append(arrA, tmp)
+		} else if amari == 1 {
+			arrB = append(arrB, tmp)
+		}
+	}
+
+	if len(arrA) <= 1 && len(arrB) <= 1 {
+		fmt.Println(-1)
+		return
+	}
+
+	sort.Ints(arrA)
+	sort.Ints(arrB)
+
+	maxA := -1
+	maxB := -1
+	if len(arrA) >= 2 {
+		maxA = arrA[len(arrA)-1] + arrA[len(arrA)-2]
+	}
+	if len(arrB) >= 2 {
+		maxB = arrB[len(arrB)-1] + arrB[len(arrB)-2]
+	}
+
+	fmt.Println(intMax(maxA, maxB))
+}
