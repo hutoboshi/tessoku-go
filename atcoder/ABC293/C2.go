23cc5e760e18a71bd2de6347879241384cbb285d
taisei <hutoboshi.eng@gmail.com>
2024-05-26 11:24:33 +0900

    dfs

---
+++ atcoder/ABC293/C2.go
@@ -0,0 +1,149 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Io struct {
+	reader    *bufio.Reader
+	writer    *bufio.Writer
+	tokens    []string
+	nextToken int
+}
+
+func NewIo() *Io {
+	return &Io{
+		reader: bufio.NewReader(os.Stdin),
+		writer: bufio.NewWriter(os.Stdout),
+	}
+}
+
+func (io *Io) Flush() {
+	err := io.writer.Flush()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (io *Io) NextLine() string {
+	var buffer []byte
+	for {
+		line, isPrefix, err := io.reader.ReadLine()
+		if err != nil {
+			panic(err)
+		}
+		buffer = append(buffer, line...)
+		if !isPrefix {
+			break
+		}
+	}
+	return string(buffer)
+}
+
+func (io *Io) Next() string {
+	for io.nextToken >= len(io.tokens) {
+		line := io.NextLine()
+		io.tokens = strings.Fields(line)
+		io.nextToken = 0
+	}
+	r := io.tokens[io.nextToken]
+	io.nextToken++
+	return r
+}
+
+func (io *Io) NextInt() int {
+	i, err := strconv.Atoi(io.Next())
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
+func (io *Io) NextFloat() float64 {
+	i, err := strconv.ParseFloat(io.Next(), 64)
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
+func (io *Io) PrintLn(a ...interface{}) {
+	fmt.Fprintln(io.writer, a...)
+}
+
+func (io *Io) Printf(format string, a ...interface{}) {
+	fmt.Fprintf(io.writer, format, a...)
+}
+
+func (io *Io) PrintIntLn(a []int) {
+	b := []interface{}{}
+	for _, x := range a {
+		b = append(b, x)
+	}
+	io.PrintLn(b...)
+}
+
+func (io *Io) PrintStringLn(a []string) {
+	b := []interface{}{}
+	for _, x := range a {
+		b = append(b, x)
+	}
+	io.PrintLn(b...)
+}
+
+func Log(name string, value interface{}) {
+	fmt.Fprintf(os.Stderr, "%s=%+v\n", name, value)
+}
+
+func intMin(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func intMax(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+var n, m int
+var a [15][15]int
+var cnt map[int]int
+var ans int
+
+func dfs(i, j int) {
+	if i < 1 || i > n || j < 1 || j > m || cnt[a[i][j]] > 0 {
+		return
+	}
+	cnt[a[i][j]]++
+	if i == n && j == m {
+		ans++
+	}
+	dfs(i+1, j)
+	dfs(i, j+1)
+	cnt[a[i][j]]--
+}
+
+func main() {
+	//決まり文句
+	io := NewIo()
+	defer io.Flush()
+
+	cnt = make(map[int]int)
+	n = io.NextInt()
+	m = io.NextInt()
+	for i := 1; i <= n; i++ {
+		for j := 1; j <= m; j++ {
+			a[i][j] = io.NextInt()
+		}
+	}
+	dfs(1, 1)
+	fmt.Println(ans)
+}
