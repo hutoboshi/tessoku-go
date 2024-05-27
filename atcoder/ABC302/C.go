d469b37aca4ce5d35f0139c0dc7f16b318396dc3
taisei <hutoboshi.eng@gmail.com>
2024-05-27 20:10:13 +0900

    再帰関数　全探索

---
+++ atcoder/ABC302/C.go
@@ -0,0 +1,165 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Io struct {
+	reader    *bufio.Reader
+	writer    *bufio.Writer
+	tokens    []string
+	nextToken int
+}
+
+func NewIo() *Io {
+	return &Io{
+		reader: bufio.NewReader(os.Stdin),
+		writer: bufio.NewWriter(os.Stdout),
+	}
+}
+
+func (io *Io) Flush() {
+	err := io.writer.Flush()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (io *Io) NextLine() string {
+	var buffer []byte
+	for {
+		line, isPrefix, err := io.reader.ReadLine()
+		if err != nil {
+			panic(err)
+		}
+		buffer = append(buffer, line...)
+		if !isPrefix {
+			break
+		}
+	}
+	return string(buffer)
+}
+
+func (io *Io) Next() string {
+	for io.nextToken >= len(io.tokens) {
+		line := io.NextLine()
+		io.tokens = strings.Fields(line)
+		io.nextToken = 0
+	}
+	r := io.tokens[io.nextToken]
+	io.nextToken++
+	return r
+}
+
+func (io *Io) NextInt() int {
+	i, err := strconv.Atoi(io.Next())
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
+func (io *Io) NextFloat() float64 {
+	i, err := strconv.ParseFloat(io.Next(), 64)
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
+func (io *Io) PrintLn(a ...interface{}) {
+	fmt.Fprintln(io.writer, a...)
+}
+
+func (io *Io) Printf(format string, a ...interface{}) {
+	fmt.Fprintf(io.writer, format, a...)
+}
+
+func (io *Io) PrintIntLn(a []int) {
+	b := []interface{}{}
+	for _, x := range a {
+		b = append(b, x)
+	}
+	io.PrintLn(b...)
+}
+
+func (io *Io) PrintStringLn(a []string) {
+	b := []interface{}{}
+	for _, x := range a {
+		b = append(b, x)
+	}
+	io.PrintLn(b...)
+}
+
+func Log(name string, value interface{}) {
+	fmt.Fprintf(os.Stderr, "%s=%+v\n", name, value)
+}
+
+func intMin(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func intMax(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func main() {
+	//決まり文句
+	io := NewIo()
+	defer io.Flush()
+
+	n := io.NextInt()
+	io.NextInt()
+
+	arrS := make([]string, n)
+	for i := 0; i < n; i++ {
+		arrS[i] = io.NextLine()
+	}
+
+	if rec(arrS, 0) {
+		fmt.Println("Yes")
+	} else {
+		fmt.Println("No")
+	}
+
+}
+
+func diff(s1 string, s2 string) int {
+	diff := 0
+	for i, v := range s1 {
+		if string(v) != string(s2[i]) {
+			diff++
+		}
+	}
+	return diff
+}
+
+func rec(ss []string, i int) bool {
+	//終了条件：文字列を使い切ったらtrue
+	if i == len(ss) {
+		return true
+	}
+	//まだ使ってない文字列を選ぶ
+	for j := i; j < len(ss); j++ {
+		if i == 0 || diff(ss[i-1], ss[j]) == 1 {
+			//要素を入れ替える
+			tmp := ss[i]
+			ss[i] = ss[j]
+			ss[j] = tmp
+			if rec(ss, i+1) {
+				return true
+			}
+		}
+	}
+	return true
+}
