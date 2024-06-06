eb17ef3b4bf49ad43e5157be0b02a9f4bad2e0c0
taisei <hutoboshi.eng@gmail.com>
2024-06-06 20:52:47 +0900

    ユーグリット距離

---
+++ atcoder/ABC330/C.go
@@ -0,0 +1,137 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Io struct {
+	reader    *bufio.Reader
+	writer    *bufio.Writer
+	tokens    []string
+	nextToken int
+}
+
+func NewIo() *Io {
+	return &Io{
+		reader: bufio.NewReader(os.Stdin),
+		writer: bufio.NewWriter(os.Stdout),
+	}
+}
+
+func (io *Io) Flush() {
+	err := io.writer.Flush()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (io *Io) NextLine() string {
+	var buffer []byte
+	for {
+		line, isPrefix, err := io.reader.ReadLine()
+		if err != nil {
+			panic(err)
+		}
+		buffer = append(buffer, line...)
+		if !isPrefix {
+			break
+		}
+	}
+	return string(buffer)
+}
+
+func (io *Io) Next() string {
+	for io.nextToken >= len(io.tokens) {
+		line := io.NextLine()
+		io.tokens = strings.Fields(line)
+		io.nextToken = 0
+	}
+	r := io.tokens[io.nextToken]
+	io.nextToken++
+	return r
+}
+
+func (io *Io) NextInt() int {
+	i, err := strconv.Atoi(io.Next())
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
+func (io *Io) NextFloat() float64 {
+	i, err := strconv.ParseFloat(io.Next(), 64)
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
+func (io *Io) PrintLn(a ...interface{}) {
+	fmt.Fprintln(io.writer, a...)
+}
+
+func (io *Io) Printf(format string, a ...interface{}) {
+	fmt.Fprintf(io.writer, format, a...)
+}
+
+func (io *Io) PrintIntLn(a []int) {
+	b := []interface{}{}
+	for _, x := range a {
+		b = append(b, x)
+	}
+	io.PrintLn(b...)
+}
+
+func (io *Io) PrintStringLn(a []string) {
+	b := []interface{}{}
+	for _, x := range a {
+		b = append(b, x)
+	}
+	io.PrintLn(b...)
+}
+
+func Log(name string, value interface{}) {
+	fmt.Fprintf(os.Stderr, "%s=%+v\n", name, value)
+}
+
+func intMin(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func intMax(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func main() {
+	//決まり文句
+	io := NewIo()
+	defer io.Flush()
+
+	d := io.NextInt()
+	ans := d
+	limit := int(math.Sqrt(float64(d))) + 9
+	for x := 0; x < limit; x++ {
+		z := d - x*x
+		if z < 0 {
+			ans = intMin(ans, -z)
+		} else {
+			y1 := int(math.Sqrt(float64(z)))
+			y2 := y1 + 1
+			ans = intMin(ans, z-y1*y1)
+			ans = intMin(ans, y2*y2-z)
+		}
+	}
+	fmt.Println(ans)
+}
