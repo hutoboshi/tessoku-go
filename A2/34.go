fcd18b9a338bf3931dc456042bcbe75a8e9b1cbf
taisei <hutoboshi.eng@gmail.com>
2024-06-25 18:27:14 +0900

    tessoku

---
+++ A2/34.go
@@ -0,0 +1,156 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Io struct {
+	reader    *bufio.Reader
+	writer    *bufio.Writer
+	tokens    []string
+	nextToken int
+}
+
+func NewIo() *Io {
+	return &Io{
+		reader: bufio.NewReader(os.Stdin),
+		writer: bufio.NewWriter(os.Stdout),
+	}
+}
+
+func (io *Io) Flush() {
+	err := io.writer.Flush()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (io *Io) NextLine() string {
+	var buffer []byte
+	for {
+		line, isPrefix, err := io.reader.ReadLine()
+		if err != nil {
+			panic(err)
+		}
+		buffer = append(buffer, line...)
+		if !isPrefix {
+			break
+		}
+	}
+	return string(buffer)
+}
+
+func (io *Io) Next() string {
+	for io.nextToken >= len(io.tokens) {
+		line := io.NextLine()
+		io.tokens = strings.Fields(line)
+		io.nextToken = 0
+	}
+	r := io.tokens[io.nextToken]
+	io.nextToken++
+	return r
+}
+
+func (io *Io) NextInt() int {
+	i, err := strconv.Atoi(io.Next())
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
+func (io *Io) NextFloat() float64 {
+	i, err := strconv.ParseFloat(io.Next(), 64)
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
+func (io *Io) PrintLn(a ...interface{}) {
+	fmt.Fprintln(io.writer, a...)
+}
+
+func (io *Io) Printf(format string, a ...interface{}) {
+	fmt.Fprintf(io.writer, format, a...)
+}
+
+func (io *Io) PrintIntLn(a []int) {
+	b := []interface{}{}
+	for _, x := range a {
+		b = append(b, x)
+	}
+	io.PrintLn(b...)
+}
+
+func (io *Io) PrintStringLn(a []string) {
+	b := []interface{}{}
+	for _, x := range a {
+		b = append(b, x)
+	}
+	io.PrintLn(b...)
+}
+
+func Log(name string, value interface{}) {
+	fmt.Fprintf(os.Stderr, "%s=%+v\n", name, value)
+}
+
+func intMin(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func intMax(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func main() {
+	//決まり文句
+	io := NewIo()
+	defer io.Flush()
+
+	n := io.NextInt()
+	x := io.NextInt()
+	y := io.NextInt()
+	arrA := make([]int, n)
+	for i := 0; i < n; i++ {
+		arrA[i] = io.NextInt()
+	}
+
+	grundy := make([]int, 100009)
+	for i := 0; i <= 100000; i++ {
+		Transit := make([]bool, 3)
+		if i >= x {
+			Transit[grundy[i-x]] = true
+		}
+		if i >= y {
+			Transit[grundy[i-y]] = true
+		}
+		if Transit[0] == false {
+			grundy[i] = 0
+		} else if Transit[1] == false {
+			grundy[i] = 1
+		} else {
+			grundy[i] = 2
+		}
+	}
+
+	XOR_Sum := 0
+	for i := 0; i < n; i++ {
+		XOR_Sum = XOR_Sum ^ grundy[arrA[i]]
+	}
+	if XOR_Sum != 0 {
+		fmt.Println("First")
+	} else {
+		fmt.Println("Second")
+	}
+}
