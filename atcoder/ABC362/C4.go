fd9d4a441ec011e68555e49d9a7ad08c58ac7453
taisei <hutoboshi.eng@gmail.com>
2024-07-14 22:17:05 +0900

    c

---
+++ atcoder/ABC362/C4.go
@@ -0,0 +1,163 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Io struct {
+	reader    *bufio.Reader
+	writer    *bufio.Writer
+	tokens    []string
+	nextToken int
+}
+
+func NewIo() *Io {
+	return &Io{
+		reader: bufio.NewReader(os.Stdin),
+		writer: bufio.NewWriter(os.Stdout),
+	}
+}
+
+func (io *Io) Flush() {
+	err := io.writer.Flush()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (io *Io) NextLine() string {
+	var buffer []byte
+	for {
+		line, isPrefix, err := io.reader.ReadLine()
+		if err != nil {
+			panic(err)
+		}
+		buffer = append(buffer, line...)
+		if !isPrefix {
+			break
+		}
+	}
+	return string(buffer)
+}
+
+func (io *Io) Next() string {
+	for io.nextToken >= len(io.tokens) {
+		line := io.NextLine()
+		io.tokens = strings.Fields(line)
+		io.nextToken = 0
+	}
+	r := io.tokens[io.nextToken]
+	io.nextToken++
+	return r
+}
+
+func (io *Io) NextInt() int {
+	i, err := strconv.Atoi(io.Next())
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
+func (io *Io) NextFloat() float64 {
+	i, err := strconv.ParseFloat(io.Next(), 64)
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
+func (io *Io) PrintLn(a ...interface{}) {
+	fmt.Fprintln(io.writer, a...)
+}
+
+func (io *Io) Printf(format string, a ...interface{}) {
+	fmt.Fprintf(io.writer, format, a...)
+}
+
+func (io *Io) PrintIntLn(a []int) {
+	b := []interface{}{}
+	for _, x := range a {
+		b = append(b, x)
+	}
+	io.PrintLn(b...)
+}
+
+func (io *Io) PrintStringLn(a []string) {
+	b := []interface{}{}
+	for _, x := range a {
+		b = append(b, x)
+	}
+	io.PrintLn(b...)
+}
+
+func Log(name string, value interface{}) {
+	fmt.Fprintf(os.Stderr, "%s=%+v\n", name, value)
+}
+
+func intMin(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func intMax(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func main() {
+	//決まり文句
+	io := NewIo()
+	defer io.Flush()
+
+	n := io.NextInt()
+	arrMin := make([]int, n)
+	arrMax := make([]int, n)
+	for i := 0; i < n; i++ {
+		arrMin[i] = io.NextInt()
+		arrMax[i] = io.NextInt()
+	}
+
+	tmp := 0
+	arrX := make([]int, n)
+	for i := 0; i < n; i++ {
+		arrX[i] = arrMin[i]
+		tmp -= arrMin[i]
+	}
+
+	if tmp < 0 {
+		fmt.Println("No")
+		return
+	}
+	for i := 0; i < n; i++ {
+		if arrMin[i]+tmp > arrMax[i] {
+			arrX[i] = arrMax[i]
+			tmp -= arrMax[i] - arrMin[i]
+		} else {
+			arrX[i] += tmp
+			tmp = 0
+		}
+
+		if tmp == 0 {
+			break
+		}
+	}
+
+	if tmp == 0 {
+		fmt.Println("Yes")
+		for _, v := range arrX {
+			fmt.Print(v, " ")
+		}
+		fmt.Println()
+	} else {
+		fmt.Println("No")
+	}
+}
