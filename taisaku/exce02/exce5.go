0aa8c5a1f1b83f9f8f570dcb4fb43b1faa8a3702
taisei <hutoboshi.eng@gmail.com>
2024-10-10 00:11:34 +0900

    二分探索練習

---
+++ taisaku/exce02/exce5.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+	binarySearch2(n)
+}
+
+func binarySearch2(n int) {
+	left := 1
+	right := 1000000
+
+	cnt := 0
+
+	for right-left+1 >= 1 {
+		fmt.Println(left, right)
+		mid := (left + right) / 2
+		fmt.Println("mid", mid)
+		midstr := strconv.Itoa(mid)
+		doubleMid, _ := strconv.Atoi(midstr + midstr)
+		if doubleMid == n {
+			cnt = mid
+			break
+		}
+		if doubleMid < n {
+			left = mid + 1
+			cnt = mid
+			fmt.Println("一致", cnt)
+		}
+		if doubleMid > n {
+			right = mid - 1
+		}
+	}
+
+	fmt.Println(cnt)
+}
