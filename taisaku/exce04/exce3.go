c0d88a93ec30e2aec66b8d2754a458251afb004e
taisei <hutoboshi.eng@gmail.com>
2024-10-14 23:56:37 +0900

    DP ナップサック問題

---
+++ taisaku/exce04/exce3.go
@@ -0,0 +1,149 @@
+// 動的計画法ナップサック問題
+// DP D
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Io struct {
+	reader    *bufio.Reader
+	writer    *bufio.Writer
+	tokens    []string
+	nextToken int
+}
+
+func NewIo() *Io {
+	return &Io{
+		reader: bufio.NewReader(os.Stdin),
+		writer: bufio.NewWriter(os.Stdout),
+	}
+}
+
+func (io *Io) Flush() {
+	err := io.writer.Flush()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (io *Io) NextLine() string {
+	var buffer []byte
+	for {
+		line, isPrefix, err := io.reader.ReadLine()
+		if err != nil {
+			panic(err)
+		}
+		buffer = append(buffer, line...)
+		if !isPrefix {
+			break
+		}
+	}
+	return string(buffer)
+}
+
+func (io *Io) Next() string {
+	for io.nextToken >= len(io.tokens) {
+		line := io.NextLine()
+		io.tokens = strings.Fields(line)
+		io.nextToken = 0
+	}
+	r := io.tokens[io.nextToken]
+	io.nextToken++
+	return r
+}
+
+func (io *Io) NextInt() int {
+	i, err := strconv.Atoi(io.Next())
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
+func (io *Io) NextFloat() float64 {
+	i, err := strconv.ParseFloat(io.Next(), 64)
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
+func (io *Io) PrintLn(a ...interface{}) {
+	fmt.Fprintln(io.writer, a...)
+}
+
+func (io *Io) Printf(format string, a ...interface{}) {
+	fmt.Fprintf(io.writer, format, a...)
+}
+
+func (io *Io) PrintIntLn(a []int) {
+	b := []interface{}{}
+	for _, x := range a {
+		b = append(b, x)
+	}
+	io.PrintLn(b...)
+}
+
+func (io *Io) PrintStringLn(a []string) {
+	b := []interface{}{}
+	for _, x := range a {
+		b = append(b, x)
+	}
+	io.PrintLn(b...)
+}
+
+func Log(name string, value interface{}) {
+	fmt.Fprintf(os.Stderr, "%s=%+v\n", name, value)
+}
+
+func intMin(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func intMax(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func main() {
+	//決まり文句
+	io := NewIo()
+	defer io.Flush()
+
+	N := io.NextInt()
+	W := io.NextInt()
+	arrW := make([]int, N)
+	arrV := make([]int, N)
+	for i := 0; i < N; i++ {
+		arrW[i] = io.NextInt()
+		arrV[i] = io.NextInt()
+	}
+
+	dp := make([][]int, N+1)
+	for i := 0; i < N+1; i++ {
+		dp[i] = make([]int, W+1)
+	}
+
+	for i := 1; i < N+1; i++ {
+		wi := arrW[i-1]
+		vi := arrV[i-1]
+		for j := 0; j < W+1; j++ {
+			if j-wi < 0 {
+				dp[i][j] = dp[i-1][j]
+			} else {
+				dp[i][j] = intMax(dp[i-1][j], dp[i-1][j-wi]+vi)
+			}
+		}
+	}
+	fmt.Println(dp[N][W])
+}
